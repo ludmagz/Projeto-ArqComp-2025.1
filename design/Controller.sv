c225cb4618530dcc2c8bdbadf746f60b4d0d32ab
Marina Rodas <122946394+marsales@users.noreply.github.com>
2025-07-23 13:29:47 -0300

    Update Controller.sv

--- design/Controller.sv
+++ design/Controller.sv
@@ -21,11 +21,11 @@ module Controller (
 
   logic [6:0] R_TYPE, I_TYPE, LW, SW, BR;
 
-  assign R_TYPE = 7'b0110011;  //add,and
-  assign I_TYPE = 7'b0010011;  //addi,subi
-  assign LW = 7'b0000011;  //lw
-  assign SW = 7'b0100011;  //sw
-  assign BR = 7'b1100011;  //beq
+  assign R_TYPE = 7'b0110011;  // ADD | AND
+  assign I_TYPE = 7'b0010011;  // ADDI | SUBI
+  assign LW = 7'b0000011;  // LW
+  assign SW = 7'b0100011;  // SW
+  assign BR = 7'b1100011;  // BEQ | BNE | BLT | BGE
 
   assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE);
   assign MemtoReg = (Opcode == LW);
