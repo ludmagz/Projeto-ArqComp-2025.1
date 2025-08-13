780ea86d1b64fdf5fe101c29eebea7754dfded2e
Marina Rodas <122946394+marsales@users.noreply.github.com>
2025-08-13 11:08:00 -0300

    Adição do logic JalrSel no RISC_V

--- design/RISC_V.sv
+++ design/RISC_V.sv
@@ -19,7 +19,7 @@ module riscv #(
 );
 
   logic [6:0] opcode;
-  logic ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, JalPrima, Halt, RWSel;
+  logic ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, JalPrima, Halt, RWSel, JalrSel;
   logic [1:0] ALUop;
   logic [1:0] ALUop_Reg;
   logic [6:0] Funct7;
